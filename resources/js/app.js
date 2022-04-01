9d7e53f6347429d099632f675d1fd0c2d0e306df
sniper <wanjaswilly@gmail.com>
2022-04-01 03:38:34 +0300

    added custom css, js & homepage

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+
+window.onscroll = function() {scrollFunction();};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "20px";
+
+  } else {
+    document.getElementById("header").style.fontSize = "50px";
+  }
+}
